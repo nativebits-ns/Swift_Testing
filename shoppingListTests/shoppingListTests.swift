fce1462175c892d4e2d98561defcb36b5bde02a1
Nachiket <nachiket.shilwant@rajasoftwarelabs.com>
2026-09-07 15:24:53 +0530

    Testing failure

--- shoppingListTests/shoppingListTests.swift
+++ shoppingListTests/shoppingListTests.swift
@@ -6,12 +6,35 @@
 //
 
 import Testing
+import SwiftData
+import Foundation
 @testable import shoppingList
-
+ 
+@MainActor
 struct shoppingListTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    let container: ModelContainer
+    let context: ModelContext
+    
+    init() throws {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        self.container = try ModelContainer(for: Item.self, configurations: config)
+        self.context = container.mainContext
     }
-
+    
+    @Test()
+    func addingItemInShoppingList() async throws {
+        let service = ShoppingListService(modelContext: context)
+        
+        try service.addItem(name: "Testing", quantity: 3)
+        
+        let descriptor = FetchDescriptor<Item>()
+        let items = try context.fetch(descriptor)
+        
+        let addedItem = try #require(items.first)
+        
+        #expect(addedItem.itemName == "Failing Job")
+        #expect(addedItem.quantity == 3)
+        #expect(addedItem.purchasedStatus == false)
+        
+    }
 }
